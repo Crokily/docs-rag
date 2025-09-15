878c46f875b6858bfb135c3245b4bc3f497765a2
Crokily <crokily@gmail.com>
2025-09-16 00:05:39 +1000

    Refactor settings to switch from Gemini to OpenAI model for LLM configuration, while retaining commented-out Gemini options for future reference.

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -3,16 +3,17 @@ import { GeminiEmbedding, Gemini, GEMINI_MODEL, GEMINI_EMBEDDING_MODEL } from "@
 import { Settings } from "llamaindex";
 
 export function initSettings() {
-  // Settings.llm = new OpenAI({
-  //   model: process.env.MODEL ?? "gpt-4o-mini",
-  //   maxTokens: process.env.LLM_MAX_TOKENS
-  //     ? Number(process.env.LLM_MAX_TOKENS)
-  //     : undefined,
-  // });
-  Settings.llm = new Gemini({
-    model: GEMINI_MODEL.GEMINI_2_0_FLASH,
+  Settings.llm = new OpenAI({
+    model: process.env.MODEL ?? "gpt-4o-mini",
+    maxTokens: process.env.LLM_MAX_TOKENS
+      ? Number(process.env.LLM_MAX_TOKENS)
+      : undefined,
     temperature: 0.1,
   });
+  // Settings.llm = new Gemini({
+  //   model: GEMINI_MODEL.GEMINI_2_5_FLASH_LITE,
+  //   temperature: 0.1,
+  // });
   Settings.embedModel = new OpenAIEmbedding({
     model: process.env.EMBEDDING_MODEL,
     dimensions: process.env.EMBEDDING_DIM
